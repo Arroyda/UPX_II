a2ee41eb666239d824c312292d0925b8ecd94dbf
§AAB§ <161661795+Draabest@users.noreply.github.com>
2024-10-03 22:25:27 -0300

    Create project 03/10

---
+++ Conta.java
@@ -0,0 +1,10 @@
+public class Conta extends Pedido {
+
+    
+
+    public Conta(int numero, String nome, int telefone) {
+        super(numero, nome, telefone);
+        
+    }
+    
+}
---
+++ GerenciadorDePedidos.java
@@ -0,0 +1,7 @@
+class GerenciadorDePedidos{
+    public static void main(String[] args) {
+        
+    }
+
+
+}
